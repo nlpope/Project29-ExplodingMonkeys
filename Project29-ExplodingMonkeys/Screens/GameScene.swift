8c676d68fe429287124577812d2ab4efb54fb1d3
Noah Pope <npope@tutanota.com>
2025-04-09 15:05:15 -0500

    adding link to video of original game

--- Project29-ExplodingMonkeys/Screens/GameScene.swift
+++ Project29-ExplodingMonkeys/Screens/GameScene.swift
@@ -1,6 +1,7 @@
 //  File: GameScene.swift
 //  Project: Project29-ExplodingMonkeys
 //  Created by: Noah Pope on 4/9/25.
+//  Original Game: https://www.youtube.com/watch?v=lyBD0X81tjk
 
 import SpriteKit
 import GameplayKit
