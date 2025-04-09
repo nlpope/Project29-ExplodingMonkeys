121fae8ac19ccc513f988e57148269ea465af8dd
Noah Pope <npope@tutanota.com>
2025-04-09 13:39:18 -0500

    Initial Commit

---
+++ Project29-ExplodingMonkeysTests/Project29_ExplodingMonkeysTests.swift
@@ -0,0 +1,14 @@
+//  File: Project29_ExplodingMonkeysTests.swift
+//  Project: Project29-ExplodingMonkeysTests
+//  Created by: Noah Pope on 4/9/25.
+
+import Testing
+@testable import Project29_ExplodingMonkeys
+
+struct Project29_ExplodingMonkeysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
